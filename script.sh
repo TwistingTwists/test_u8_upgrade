fb2bca813ae0debeee95cd179fbef5e2aee91162
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-07-12 15:08:18 +0530

    scipt

--- script.sh
+++ script.sh
@@ -4,6 +4,8 @@ dfx canister create test_u8_upgrade_backend
 
 dfx build
 
-candid-extractor target/wasm32-unknown-unknown/release/test_u8_upgrade_backend.wasm > ./src/test_u8_upgrade_backend/test_u8_upgrade_backend.did
+# candid-extractor target/wasm32-unknown-unknown/release/test_u8_upgrade_backend.wasm > ./src/test_u8_upgrade_backend/test_u8_upgrade_backend.did
+#
+candid-extractor .dfx/local/canisters/test_u8_upgrade_backend/test_u8_upgrade_backend.wasm  > ./src/test_u8_upgrade_backend/test_u8_upgrade_backend.did
 
-dfx canister install test_u8_upgrade_backend 
+dfx canister install test_u8_upgrade_backend
